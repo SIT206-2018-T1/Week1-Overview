6622e6eb1094934c7ef56a21c0e8a2d979e1a49e
darkeung <hawygfau@gmail.com>
2018-09-10 13:57:29 +1000

    wk1

---
+++ week1/week1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week1
+//
+//  Created by NG CHUN KEUNG on 28/5/18.
+//  Copyright © 2018 NG CHUN KEUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
